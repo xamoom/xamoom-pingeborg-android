0625efd5459c52b1d7ac1939151a4dfd8240e0d9
Raphael Seher <raphael@xamoom.com>
2015-07-06 09:25:43 +0200

    Added title and image to AboutPage

--- xamoom-pingeborg-android/app/src/main/java/com/xamoom/android/xamoom_pingeborg_android/AboutFragment.java
+++ xamoom-pingeborg-android/app/src/main/java/com/xamoom/android/xamoom_pingeborg_android/AboutFragment.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 
 import com.xamoom.android.APICallback;
 import com.xamoom.android.XamoomEndUserApi;
+import com.xamoom.android.mapping.ContentBlocks.ContentBlockType0;
+import com.xamoom.android.mapping.ContentBlocks.ContentBlockType3;
 import com.xamoom.android.mapping.ContentById;
 import com.xamoom.android.xamoomcontentblocks.XamoomContentFragment;
 
@@ -77,6 +79,13 @@ public class AboutFragment extends android.support.v4.app.Fragment {
         XamoomEndUserApi.getInstance().getContentById("f0da3d3d28d3418e9ccc4a6e9b3493c0", false, false, null, new APICallback<ContentById>() {
             @Override
             public void finished(ContentById result) {
+
+                ContentBlockType0 title = new ContentBlockType0(result.getContent().getTitle(),true, 0, result.getContent().getDescriptionOfContent());
+                ContentBlockType3 image = new ContentBlockType3(null, true, 3, result.getContent().getImagePublicUrl());
+
+                result.getContent().getContentBlocks().add(0, title);
+                result.getContent().getContentBlocks().add(1, image);
+
                 XamoomContentFragment fragment = XamoomContentFragment.newInstance(null, Config.YOUTUBE_API_KEY);
                 fragment.setContentBlocks(result.getContent().getContentBlocks());
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.aboutContentFrameLayout, fragment).commit();
