0bcd45582004fabe5660e0f238078472da73d0d9
Raphael Seher <raphael@xamoom.com>
2017-06-13 14:01:43 +0200

    Update gradle & xamoom sdk

--- xamoom-pingeborg-android/app/src/main/java/com/xamoom/android/xamoom_pingeborg_android/XamoomPingeborgApp.java
+++ xamoom-pingeborg-android/app/src/main/java/com/xamoom/android/xamoom_pingeborg_android/XamoomPingeborgApp.java
@@ -12,57 +12,53 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.NotificationCompat;
 import com.xamoom.android.xamoomsdk.EnduserApi;
 
-
-/**
- * Created by raphaelseher on 25.11.15.
- */
 public class XamoomPingeborgApp extends Application {
-    private static final String TAG = XamoomPingeborgApp.class.getSimpleName();
-    private static final String MAJOR_ID = "52414";
-    private static final int BEACON_NOTIFICATION_ID = 1;
-    public static final String BEACON_NOTIFICATION = "BEACON_NOTIFICATION";
+  private static final String TAG = XamoomPingeborgApp.class.getSimpleName();
+  private static final String MAJOR_ID = "52414";
+  private static final int BEACON_NOTIFICATION_ID = 1;
+  public static final String BEACON_NOTIFICATION = "BEACON_NOTIFICATION";
 
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    EnduserApi.getSharedInstance(getResources().getString(R.string.apiKey), getApplicationContext());
+    XamoomBeaconService.getInstance(getApplicationContext()).startBeaconService(MAJOR_ID);
+
+    LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mEnterRegionBroadCastReciever, new IntentFilter(XamoomBeaconService.ENTER_REGION_BROADCAST));
+    LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mExitRegionBroadCastReciever, new IntentFilter(XamoomBeaconService.EXIT_REGION_BROADCAST));
+  }
+
+  private final BroadcastReceiver mEnterRegionBroadCastReciever = new BroadcastReceiver() {
     @Override
-    public void onCreate() {
-        super.onCreate();
-        EnduserApi.getSharedInstance(getResources().getString(R.string.apiKey));
-        XamoomBeaconService.getInstance(getApplicationContext()).startBeaconService(MAJOR_ID);
+    public void onReceive(Context context, Intent intent) {
+      Intent activityIntent = new Intent(XamoomPingeborgApp.this, MainActivity.class);
+      activityIntent.putExtra(BEACON_NOTIFICATION, true);
+      activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mEnterRegionBroadCastReciever, new IntentFilter(XamoomBeaconService.ENTER_REGION_BROADCAST));
-        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mExitRegionBroadCastReciever, new IntentFilter(XamoomBeaconService.EXIT_REGION_BROADCAST));
+      PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+              activityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+      NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext());
+      notificationBuilder
+              .setContentTitle(getString(R.string.beacon_notification_title))
+              .setContentText(getString(R.string.beacon_notification_text))
+              .setContentInfo(getString(R.string.beacon_notification_info))
+              .setSmallIcon(R.drawable.ic_ble_notification)
+              .setContentIntent(pendingIntent);
+
+      Notification notification = notificationBuilder.build();
+      //notification.flags |= Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;
+
+      NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+      notificationManager.notify(BEACON_NOTIFICATION_ID, notification);
     }
+  };
 
-    private final BroadcastReceiver mEnterRegionBroadCastReciever = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Intent activityIntent = new Intent(XamoomPingeborgApp.this, MainActivity.class);
-            activityIntent.putExtra(BEACON_NOTIFICATION, true);
-            activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-                    activityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext());
-            notificationBuilder
-                    .setContentTitle(getString(R.string.beacon_notification_title))
-                    .setContentText(getString(R.string.beacon_notification_text))
-                    .setContentInfo(getString(R.string.beacon_notification_info))
-                    .setSmallIcon(R.drawable.ic_ble_notification)
-                    .setContentIntent(pendingIntent);
-
-            Notification notification = notificationBuilder.build();
-            //notification.flags |= Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;
-
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
-            notificationManager.notify(BEACON_NOTIFICATION_ID, notification);
-        }
-    };
-
-    private final BroadcastReceiver mExitRegionBroadCastReciever = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
-            notificationManager.cancel(BEACON_NOTIFICATION_ID);
-        }
-    };
+  private final BroadcastReceiver mExitRegionBroadCastReciever = new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+      notificationManager.cancel(BEACON_NOTIFICATION_ID);
+    }
+  };
 }
